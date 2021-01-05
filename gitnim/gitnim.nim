0de7276e1cd069209a4c3f203ebf51847d5acf65
Andy Davidoff <github@andy.disruptek.com>
2021-01-05 00:18:37 -0500

    nightly 2021-01-04

--- gitnim/gitnim.nim
+++ gitnim/gitnim.nim
@@ -4,7 +4,6 @@ import std/os
 import std/strutils
 import std/osproc
 import std/logging
-import std/uri
 
 import cutelog
 
@@ -14,30 +13,11 @@ const
   embURL: string = staticExec"git remote get-url origin"
   URL {.strdefine.}: string = getEnv(envURL, embURL)
 
-# use the above to guess the distribution URL
-proc toDist(u: string): string {.compileTime.} =
-  var url = parseUri(u)
-  url.path = parentDir(url.path) / "dist"
-  result = $url
-
-const
-  distURL: string = "NIM_DIST"
-  embDist: string = embURL.toDist
-  dist {.strdefine.}: string = getEnv(distURL, embDIST)
-
 static:
   hint "gitnim uses following url by default:"
   hint URL
-  hint "via setting the $" & envURL & ", or"
+  hint "via setting the $GITNIM_URL, or"
   hint "via passing --define:URL=\"...\""
-  hint "gitnim uses the following distribution url:"
-  hint dist
-  hint "via setting the $" & distURL & ", or"
-  hint "via passing --define:dist=\"...\""
-
-template distribution(): string =
-  ## the current distribution URL
-  getEnv(distURL, dist)
 
 type
   RunOutput = object
@@ -52,26 +32,6 @@ macro repo(): untyped =
 template crash(why: string) {.used.} =
   error why
   quit 1
-
-template withinDirectory(path: typed; body: untyped) =
-  ## do something within a particular directory
-  if dirExists $(path):
-    let cwd = getCurrentDir()
-    setCurrentDir $(path)
-    try:
-      body
-    finally:
-      setCurrentDir cwd
-  else:
-    raise newException(ValueError, $path & " is not a directory")
-
-template withinDistribution(body: untyped) =
-  ## do something within the distribution directory
-  let dist = getAppDir().parentDir / "dist"
-  if dirExists dist:
-    if fileExists dist / ".git":
-      withinDirectory dist:
-        body
 
 proc run(exe: string; args: openArray[string];
          options = defaultProcess): RunOutput =
@@ -95,13 +55,15 @@ proc run(exe: string; args: openArray[string];
       # the user wants interactivity
       when defined(debug):
         debug command, arguments.join(" ")
-      let process = startProcess(command, getAppDir(),
-                                 args = arguments, options = opts)
+      let
+        process = startProcess(command, $getAppDir(),
+                               args = arguments, options = opts)
       result = RunOutput(ok: process.waitForExit == 0)
     else:
-      var execute = join(@[command] & arguments, " ")
-      let (output, code) = execCmdEx(execute, options = opts)
-      result = RunOutput(ok: code == 0, output: output)
+      let
+        output = execProcess(command, $getAppDir(),
+                             args = arguments, options = opts)
+      result = RunOutput(ok: true, output: output)
 
     # for utility, also return the arguments we used
     result.arguments = arguments
@@ -112,62 +74,28 @@ proc run(exe: string; args: openArray[string];
     when defined(debug):
       debug "done running"
 
-proc git(args: string; options = defaultProcess): string
-  {.discardable.} =
-  ## run git with some arguments
-  run("git", args.split, options = options).output
-
 proc git(args: openArray[string]; options = defaultProcess): string
   {.discardable.} =
-  ## run git with some arguments
   run("git", args, options = options).output
 
 proc nim(args: openArray[string]; options = defaultProcess): string =
-  ## run nim with some arguments
   run("nim", args, options = options).output
 
-proc refresh() =
-  discard git("fetch --all")
-  withinDirectory getAppDir().parentDir:
-    let dist = "dist"
-    if not dirExists dist:
-      createDir dist
-      git("submodule add --depth 1 $1 dist" % [
-        distribution().quoteShell ])
-      echo "added submodule: ", distribution()
-  withinDistribution:
-    echo "fetch and pull distribution"
-    git("fetch --all --prune")
-    git("pull")
-
-proc switch(branch: string): bool =
-  ## switch compiler and distribution branches
-  withinDirectory getAppDir().parentDir:
-    let switch = run("git", ["checkout", branch], {poStdErrToStdOut})
-    result = switch.ok
-    if result:
-      info "using the $1 compiler branch" % [ branch ]
-      withinDistribution:
-        if run("git", ["checkout", branch], {poStdErrToStdOut}).ok:
-          info "using the $1 distribution branch" % [ branch ]
-          return
-      warn "the $1 distribution branch is not available" % [ branch ]
-    else:
-      warn switch.output
-
 when isMainModule:
-  let logger = newCuteConsoleLogger()
+  let
+    logger = newCuteConsoleLogger()
   addHandler logger
 
-  let app = extractFilename getAppFilename()
-  info "$1 on $2 for Nim $3" % [ app, repo(), NimVersion ]
+  let
+    app = extractFilename getAppFilename()
+  info app & " on " & repo() & " for Nim " & NimVersion
 
   if paramCount() == 0:
-    refresh()
+    discard git("fetch --all --prune".split, {poStdErrToStdOut})
     info "specify a branch; eg. `git nim 1.2.2` or `git nim origin/1.0.7`:"
-    info git("branch --all --sort=version:refname --verbose --color", {poStdErrToStdOut})
+    info git("branch --all --sort=version:refname --verbose --color".split, {poStdErrToStdOut})
     info "or you can specify one of these tags; eg. `git nim latest`:"
-    info git("tag --list -n2 --sort=version:refname --color", {poStdErrToStdOut})
+    info git("tag --list -n2 --sort=version:refname --color".split, {poStdErrToStdOut})
   else:
-    if switch paramStr(1):
-      info nim ["--version"]
+    git ["checkout", paramStr(1)]
+    info nim ["--version"]
